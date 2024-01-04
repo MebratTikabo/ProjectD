32ce7e464ccc37100bd846e528300ce5180f021e
Habteab <Habteab@habteab>
2024-01-03 21:22:28 -0500

    initial Commit

---
+++ src/d/DemoD.java
@@ -0,0 +1,10 @@
+package d;
+
+public class DemoD {
+public static void main(String[] args) {
+	
+	System.out.println("git practice D");
+	System.out.println(" it is working ");
+	System.out.println(" the repo is good");
+}
+}
